83b132d5f6680357641795b463093129e15e7217
Nazar Mihonko <migonkonazar@gmail.com>
2026-05-11 00:06:50 +0300

    Fix git push

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -1,4 +1,17 @@
 package app;
 
 public class Corrector {
+
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1;
+
+        for (String str : strs) {
+            str = str.replace("b", "o");
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+            count++;
+        }
+
+        return stringBuilder.toString();
+    }
 }
--- src/app/DataProvider.java
+++ src/app/DataProvider.java
@@ -1,4 +1,23 @@
 package app;
 
+import java.util.Scanner;
+
 public class DataProvider {
+
+    public String[] getData() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many words do you want to enter? ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] words = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("Enter your words " + (i + 1) + ": ");
+            words[i] = scanner.nextLine();
+        }
+
+        return words;
+    }
 }
